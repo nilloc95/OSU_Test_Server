6a1bdab9abf52b7b06a64e8ab9c1f17a8c7f2eea
Collin Gilmore <collingilmore@gmail.com>
2021-05-10 20:41:00 -0400

    testing

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-var http = require('http');
+var express = require('express');
 
-http.createServer(function(req,res){
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world!');
-}).listen(3000);
+var app = express();
 
-console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+app.set('port', 3000);
+
+app.get('/',function(req,res){
+  res.type('text/plain');
+  res.send('Welcome to the main page!');
+});
+
+app.get('/other-page',function(req,res){
+  res.type('text/plain');
+  res.send('Welcome to the other page!');
+});
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
